f4512caafe834c4240c0e140e933083618117b29
Marc <misertej@gmail.com>
2017-06-07 18:06:32 -0700

    Last commit was old version of the file. Here is the last one.

--- javascriptLibraryMarc.js
+++ javascriptLibraryMarc.js
@@ -1,23 +1,59 @@
 var _ = {
-   map: function() {
-     //code here;
-   },
-   reduce: function() { 
-     // code here;
-   },
-   find: function() {   
-     // code here;
-   },
-   filter: function(array, callback) { 
-       for(i = 0; i < array.length; i++){
-        callback(array[i]);
-       }
-   },
-   reject: function() { 
-     // code here;
+  map: function(arr, callback) {
+    for(i = 0; i < arr.length; i++){
+        var result = callback(arr[i]);
+        arr[i] = result;
+    }
+    return arr;
+  },
+  reduce: function(arr, callback) {
+   var memo = 0;
+   for (var i= 0; i < arr.length - 1; i++){
+       var results = callback(arr[i], arr[arr.length-1]);
+       memo += arr[i];
    }
- }
-// you just created a library with 5 methods!
+   var num = arr[arr.length -1];
+   return memo + num;
+   
+  },
+  find: function(arr, callback) {  
+    for(i = 0; i < arr.length; i++){
+        var result = callback(arr[i]);
+        if(result){
+            return arr[i];
+        }
+    }
+  },
+  filter: function(arr, callback) {
+   var returnArr = [];
+    for(i = 0; i < arr.length; i++){
+        var result = callback(arr[i]);
+        if(result){
+            returnArr.push(arr[i]);
+        }
+    }
+    return returnArr;
+  },
+  reject: function(arr, callback) {
+   //Made without creating a temp array
+    for(i = 0; i < arr.length; i++){
+       var result = callback(arr[i]);
+       if(result){
+           arr.splice(i,1);
+       }
+    }
+    return arr;
+  }
+ 
+}
 
 var evens = _.filter([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
-console.log(evens); // if things are working right, this will return [2,4,6].````
+var odds = _.reject([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+var detect = _.find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+var collect = _.map([1, 2, 3, 4, 5, 6], function(num){ return num * 3 });
+var inject = _.reduce([1, 2, 3], function(memo, num){ return memo + num });
+console.log(evens);
+console.log(odds);
+console.log(detect);
+console.log(collect);
+console.log(inject);
